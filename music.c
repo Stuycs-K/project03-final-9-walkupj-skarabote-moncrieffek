b8761b1a54da5a622020af71732400dc4e5b004e
jowalkup <jwalkup40@stuy.edu>
2024-01-05 14:48:28 -0500

    made plan for bass, will implement this weekend

--- music.c
+++ music.c
@@ -1,2 +1,8 @@
 #include "bass.h"
 #include <stdio.h>
+
+// make a basic thing to get a filename (song name?) from user
+// send that over to Bass
+// 1st step: play one thing
+// 2nd step: loop
+// also update readme to say u need to download bass

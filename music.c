8eb2045aec033f089447bdead31979e412e2f58c
jowalkup <jwalkup40@stuy.edu>
2024-01-07 20:10:19 -0500

    added bass to the repo (idk if i needed to, can delete later)

--- music.c
+++ music.c
@@ -1,4 +1,5 @@
-#include "bass.h"
+#include "bass24-osx/bass.h"
+#include<unistd.h>
 #include <stdio.h>
 
 // make a basic thing to get a filename (song name?) from user
@@ -6,3 +7,22 @@
 // 1st step: play one thing
 // 2nd step: loop
 // also update readme to say u need to download bass
+
+void startMusic () {
+HMUSIC hm;
+	BASS_Init (-1, 44100, 0, 0, NULL);
+	hm=BASS_MusicLoad(FALSE,"music.mp3",0,0,BASS_SAMPLE_LOOP,0);
+	BASS_ChannelPlay(hm,FALSE);
+}
+
+void endMusic() {
+	BASS_Stop();
+	BASS_Free();
+}
+
+int main(){
+  printf("well i sure hope this works!\n");
+  startMusic();
+  sleep(100);
+  endMusic();
+}

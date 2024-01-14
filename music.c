21ecb92d6f557130cb5b3a13376d5a9ef0117253
Jo Walkup <jwalkup40@stuy.edu>
2024-01-14 16:54:21 -0500

    fixed delay

--- music.c
+++ music.c
@@ -47,9 +47,9 @@ void play_library(struct song_node** lib){
         struct song_node* letter_list = lib[letter];
         if(lib[i]){
             play(lib[i]->name);
+		sleep(120);
         }
         letter++;
-				sleep(120);
     }
 }
 
